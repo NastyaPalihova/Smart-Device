23bcf136afda6ac44151dd5c085e2a0686dd0ffa
muskratysh <muskratysh@gmail.com>
2023-03-07 21:19:31 +0300

    Реализует хедер и сверяет с ПП на 3 состояниях

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {StickyHeader} from './utils/sticky-header';
 
 // ---------------------------------
 
@@ -10,6 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  StickyHeader();
 
   // Modules
   // ---------------------------------
