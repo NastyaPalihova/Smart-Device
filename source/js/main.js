a76992773cc2f91eec6986649a094b537892073a
muskratysh <muskratysh@gmail.com>
2023-03-09 23:53:33 +0300

    Реализует аккордеон для мобайла

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccordion} from './modules/accordion';
 
 // ---------------------------------
 
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   iosVhFix();
 
+
   // Modules
   // ---------------------------------
 
@@ -18,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+    initAccordion();
     const form = new Form();
     window.form = form;
     form.init();
