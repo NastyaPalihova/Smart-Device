97ce9373d6abf6314b6f0601791cb5dac0541078
muskratysh <muskratysh@gmail.com>
2023-03-12 15:12:43 +0300

    Реализует О компании в мобайле

---
+++ source/js/modules/init-change-text.js
@@ -0,0 +1,13 @@
+// export const initChangeText = () => {
+//   if (window.innerWidth < 320) {
+//     const h2 = document.querySelector('.catalog h2');
+//     h2.innerText = 'Товары и услуги Smart Device';
+//   } else {
+//     const h2 = document.querySelector('.catalog h2');
+//     h2.innerText = 'Smart Device предлагает следующие товары и услуги';
+//   }
+// };
+
+// window.addEventListener('resize', initChangeText);
+
+// export default initChangeText;
