9fa4bbd6c87f92c2a45e07c20f1672c47f44321e
muskratysh <muskratysh@gmail.com>
2023-03-15 14:23:14 +0300

    исправляет ошибки после код-ревью

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -2,27 +2,29 @@ const accordions = document.querySelectorAll('.accordion');
 const accordionToggles = document.querySelectorAll('.accordion__toggle');
 
 export const initAccordion = () => {
-  accordions.forEach((accordion, index) => {
-    const accordionToggle = accordionToggles[index];
-    accordion.classList.remove('accordion--no-js');
-    accordionToggle.addEventListener('click', function () {
-      if (accordion.classList.contains('accordion--opened')) {
-        accordion.classList.remove('accordion--opened');
-        accordion.classList.add('accordion--closed');
-      } else {
-        // Close all other accordions
-        accordions.forEach((otherAccordion) => {
-          if (otherAccordion !== accordion && otherAccordion.classList.contains('accordion--opened')) {
-            otherAccordion.classList.remove('accordion--opened');
-            otherAccordion.classList.add('accordion--closed');
-          }
-        });
+  if (accordions.length && accordionToggles.length) {
+    accordions.forEach((accordion, index) => {
+      const accordionToggle = accordionToggles[index];
+      accordion.classList.remove('accordion--no-js');
+      accordionToggle.addEventListener('click', function () {
+        if (accordion.classList.contains('accordion--opened')) {
+          accordion.classList.remove('accordion--opened');
+          accordion.classList.add('accordion--closed');
+        } else {
+          // Close all other accordions
+          accordions.forEach((otherAccordion) => {
+            if (otherAccordion !== accordion && otherAccordion.classList.contains('accordion--opened')) {
+              otherAccordion.classList.remove('accordion--opened');
+              otherAccordion.classList.add('accordion--closed');
+            }
+          });
 
-        accordion.classList.add('accordion--opened');
-        accordion.classList.remove('accordion--closed');
-      }
+          accordion.classList.add('accordion--opened');
+          accordion.classList.remove('accordion--closed');
+        }
+      });
     });
-  });
+  }
 };
 
 export default initAccordion;
--- source/js/modules/init-more-text.js
+++ source/js/modules/init-more-text.js
@@ -2,15 +2,17 @@ const button = document.querySelector('.company__button');
 const moreText = document.querySelector('.company__more');
 
 export const initMoreText = () => {
-  button.addEventListener('click', () => {
-    if (moreText.classList.contains('company__more--hidden')) {
-      moreText.classList.remove('company__more--hidden');
-      button.textContent = 'Свернуть';
-    } else {
-      moreText.classList.add('company__more--hidden');
-      button.textContent = 'Подробнее';
-    }
-  });
+  if (button && moreText) {
+    button.addEventListener('click', () => {
+      if (moreText.classList.contains('company__more--hidden')) {
+        moreText.classList.remove('company__more--hidden');
+        button.textContent = 'Свернуть';
+      } else {
+        moreText.classList.add('company__more--hidden');
+        button.textContent = 'Подробнее';
+      }
+    });
+  }
 };
 
 export default initMoreText;
